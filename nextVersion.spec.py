42abaf455c87465e69e398aa168e58b6b67d2414
dhaberst <daniel.haberstock1@gmail.com>
2018-09-20 16:43:02 -0600

    Added template tests for nextVersion :cat2:.

---
+++ nextVersion.spec.py
@@ -0,0 +1,15 @@
+import unittest
+from nextVersion import nextVersion
+
+class TestNextVersion(unittest.TestCase):
+    def testNextVersionFunction(self):
+        pass
+    
+    def testNextVersionInvalidArguments(self):
+        pass
+
+    def testNextVersionEdgeCases(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
