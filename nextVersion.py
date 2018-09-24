3791a413292c2a94c8aba3e8d1af8219be9f64ac
dhaberst <daniel.haberstock1@gmail.com>
2018-09-23 20:34:58 -0600

    Fixed the issue where the first number > 10 would not work :goat:.

--- nextVersion.py
+++ nextVersion.py
@@ -25,22 +25,27 @@ def nextVersion(version):
     if not bool(re.match('^[0-9\.]*$', version)):
         exit(-1)
 
+    # Here we need to count the number of periods (never changes)
+    periods = version.count('.')
+
     # For simplicity we strip out the periods and rejoin as a string
     number = ''.join(version.split("."))
-
-    # We need to store the length for the future
-    length = len(number)
 
     # Here we can increment the number now by converting to int first
     number = int(number) + 1
 
-    # Now we convert it back to a version number
+    # Now we convert it back to a str so we can manipulate it
     strNumber = str(number)
-    newVersion = '.'.join(list(strNumber))
 
-    # If the length increases then we know the first number > 9,
-    #  therefore we can safely remove the first occuring period
-    if len(strNumber) > length:
-        newVersion = newVersion.replace('.', '', 1)
+    # Here's the tricky part:
+    #  Since we know how many periods there are and that they don't change
+    #  we can simply join WITH PERIODS the last x (period count) numbers 
+    #  with the first untouched numbers, also including a periodd in the middle.
+    #  We need to also check if it is a single number otherwise we don't
+    #  need to include the period.
+    if len(strNumber) > 1:
+        newVersion = strNumber[0:len(strNumber) - periods] + '.' + '.'.join(strNumber[len(strNumber) - periods:len(strNumber)])
+    else:
+        return '.'.join(strNumber)
 
     return newVersion
